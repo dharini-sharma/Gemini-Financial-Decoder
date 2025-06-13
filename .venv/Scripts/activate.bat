11cb15877623797da8f2de60264aa1582b2f183f
user-git18 <dharinisharma.la2023@vitstudent.ac.in>
2025-06-13 13:21:27 +0530

    Cleaned up ignored files from tracking

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\23BAI1263\Gen AI course\Gemini Pro\.venv
+set VIRTUAL_ENV=D:\Dharini\GenAI\Gemini_Financial_Project\Gemini-Pro-Financial-Decoder\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
